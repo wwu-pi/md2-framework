6a4e3b4bab6052f6c5a5020e44bf7575ff405390
Dexter-03 <tobias.reischmann@uni-muenster.de>
2014-11-10 14:44:41 +0100

    Added a ModelProvider to easily load models from file.

---
+++ de.wwu.md2.framework.tests/src/de/wwu/md2/framework/tests/utils/ModelProvider.java
@@ -0,0 +1,41 @@
+package de.wwu.md2.framework.tests.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+
+/**
+ * In this class all MD2 models should be listed. Their path should be inserted
+ * as a static string with a '/' separator for the packages. Using it as a
+ * static extension the models can me loaded into the tests using
+ * STATIC_MODEL_STRING.load;
+ * 
+ * @author Tobias Reischmann
+ * 
+ */
+public class ModelProvider {
+
+	private static String BASE_URI = System.getProperty("user.dir")
+			+ "/src/de/wwu/md2/framework/tests/";
+
+	public static String MY_MODEL = "dsl/controller/test.md2";
+
+	/**
+	 * Load the model from file.
+	 * @param modelUri the model URIs are listed in the ModelProvider as static Srings.
+	 * @return md2 model as a string.
+	 * @throws IOException
+	 */
+	public static String load(String modelUri) throws IOException {
+		String model;
+		FileInputStream inputStream = new FileInputStream(BASE_URI + modelUri);
+		try {
+			model = IOUtils.toString(inputStream);
+		} finally {
+			inputStream.close();
+		}
+		return model;
+	}
+
+}
