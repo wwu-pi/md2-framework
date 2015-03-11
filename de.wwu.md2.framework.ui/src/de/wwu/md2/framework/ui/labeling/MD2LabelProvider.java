6f5264093790f640191aee0b300eb7a4e722fdec
Dexter-03 <tobias.reischmann@uni-muenster.de>
2015-03-11 16:38:48 +0100

    Changed exceptions due to jenkins incapable of source level 1.7

--- de.wwu.md2.framework.ui/src/de/wwu/md2/framework/ui/labeling/MD2LabelProvider.java
+++ de.wwu.md2.framework.ui/src/de/wwu/md2/framework/ui/labeling/MD2LabelProvider.java
@@ -3,7 +3,6 @@
 */
 package de.wwu.md2.framework.ui.labeling;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
@@ -31,7 +30,7 @@ public class MD2LabelProvider extends DefaultEObjectLabelProvider {
 		try {
 			getNameMethod = ele.getClass().getMethod("getName");
 			return (String) getNameMethod.invoke(ele) + " <"+ele.getClass().getInterfaces()[0].getSimpleName()+">";
-		} catch (NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e1) {
+		} catch (Exception e1) {
 			return "<"+ele.getClass().getInterfaces()[0].getSimpleName()+">";
 		}
 	}
